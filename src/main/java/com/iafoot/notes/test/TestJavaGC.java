f9ee0b264e595ab29897cbabf7b345cbe0c30a81
hanruixian <1170807870@qq.com>
2021-01-19 14:04:14 +0800

    堆是分配对象存储的唯一选择吗？ begin

--- src/main/java/com/iafoot/notes/test/TestJavaGC.java
+++ src/main/java/com/iafoot/notes/test/TestJavaGC.java
@@ -7,6 +7,17 @@ import java.util.List;
 
 /**
  * -Xms60m -Xmx60m -XX:NewRatio=2 -XXSurvivorRatio=8 -XX:+PrintGCDetails
+ *  -XX:+PrintFlagsInitial：查看所有参数的默认值。
+ *     * -XX:+PrintFlagFinal：查看所有的参数的最终值（可能会存在修改，不再是默认值）。
+ *     * -Xms：初始堆空间内存（默认是物理内存的1/64）。
+ *     * -Xmx：最大堆空间内存（默认物理内存的1/4）。
+ *     * -Xmn：设置新生代的大小。（初始值及最大值）。
+ *     * -XX: NewRatio：配置新生代与老年代在堆结构的占比。
+ *     * -XX:SurvivorRatio：设置新生代中Eden和s0/s1空间的比例。
+ *     * -XX:MaxTenuringThreshold：设置新生代垃圾的最大年龄。
+ *     * -XX:+PrintGCDetails：输出详细的GC处理日志
+ *         * 打印gc简要信息：-XX:+PrintGC。-verbose:gc。
+ *     * -XX:HandlePromotionFailure：是否设置空间分配担保。
  */
 public class TestJavaGC {
     public static void main(String[] args) throws Exception{
