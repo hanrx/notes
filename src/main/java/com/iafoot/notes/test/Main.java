df611973a0267cd6ab59cd653288b6a353710963
hanruixian <1170807870@qq.com>
2021-01-28 20:01:46 +0800

    Thread类的有关方法

--- src/main/java/com/iafoot/notes/test/Main.java
+++ src/main/java/com/iafoot/notes/test/Main.java
@@ -32,3 +32,12 @@ public class Main {
 
 
 }
+class MyThread extends Thread{
+    @Override
+    public void run() {
+        super.run();
+        for (int i = 0; i <100 ; i++) {
+            Thread.currentThread().getName();
+        }
+    }
+}
