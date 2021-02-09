efe36b8bf7ebb7eb19e9bef2b9844841f4565563
hanruixian <1170807870@qq.com>
2021-02-09 17:10:16 +0800

    Configuration

--- src/main/java/com/iafoot/notes/NotesApplication.java
+++ src/main/java/com/iafoot/notes/NotesApplication.java
@@ -8,6 +8,22 @@ public class NotesApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(NotesApplication.class, args);
+/*
+
+        //打印所有容器中bean。
+        //1. 返回我们IOC容器
+        ConfigurableApplicationContext run = SpringApplication.run(NotesApplication.class, args);
+        //2.查看容器里面的组件。
+        String[] names = run.getBeanDefinitionNames();
+        for (String name : names) {
+            System.out.println("容器中bean名字："+name);
+        }
+        // 3.从容器中获取组件
+        ScannerTest scannerTest = run.getBean("scannerTest",ScannerTest.class);
+*/
+
+
+
     }
 
 }
